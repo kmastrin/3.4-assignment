9bcf389dc18b4b957144c2b3108446a28d9a96d3
kmastrin <kmastrin@student.alamo.edu>
2023-10-02 18:45:25 -0500

    index

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const name = "Kam";
+const name = "It's Bang Chan's birthday today!";
 
-function stay() {
-    console.log("Stray Kids Won Best Kpop at the VMAs");
+function upper(text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
 }
 
-stay();
+upper(name);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(4,4);
+
+function group() {
+    console.log('Stray Kids Members:');
+    console.log('Bang Chan');
+    console.log('Lee Know');
+    console.log('Seo Changbin');
+    console.log('Hwang Hyunjin');
+    console.log('Jisung Han');
+    console.log('Lee Felix');
+    console.log('Kim Seungmin');
+    console.log('Yang Jeongin');
+}
+
+group();
+
+var maknae = {
+    firstName: "Jeongin",
+    lastName: "Yang",
+    age: 22,
+    getFullName: function() {
+        return this.firstName + '' + this.lastName
+    }
+};
